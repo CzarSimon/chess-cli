cd2dbb6feeea889684f6a401fb4956419b0b4432
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-12-22 23:31:32 +0100

    Improved build script to add version

--- scripts/build-cli.sh
+++ scripts/build-cli.sh
@@ -1,1 +1,15 @@
-go build -o chess cmd/cli/main.go
+NAME=`cat package.json | jq .name -r`
+VERSION=`cat package.json | jq .version -r`
+COMMIT=`git rev-parse --short HEAD`
+
+FULL_VERSION="v$VERSION ($COMMIT)"
+
+sed s/'appVersion = ".*"'/"appVersion = \"$FULL_VERSION\""/g cmd/cli/main.go | gofmt > cmd/cli/main.tmp.go
+
+cp main.go /tmp/nlp-main.go
+mv cmd/cli/main.go "/tmp/$NAME-$VERSION-main.go"
+
+go build -o chess cmd/cli/main.tmp.go
+
+rm cmd/cli/main.tmp.go
+mv "/tmp/$NAME-$VERSION-main.go" cmd/cli/main.go

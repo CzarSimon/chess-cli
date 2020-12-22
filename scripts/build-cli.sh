a82418f93967b91bd32badc020b45cde216db4c3
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-12-22 23:35:09 +0100

    fixed versioning in build script

--- scripts/build-cli.sh
+++ scripts/build-cli.sh
@@ -4,12 +4,10 @@ COMMIT=`git rev-parse --short HEAD`
 
 FULL_VERSION="v$VERSION ($COMMIT)"
 
-sed s/'appVersion = ".*"'/"appVersion = \"$FULL_VERSION\""/g cmd/cli/main.go | gofmt > cmd/cli/main.tmp.go
+sed s/'appVersion = ".*"'/"appVersion = \"$FULL_VERSION\""/g internal/cli/cli.go | gofmt > internal/cli/cli.tmp.go
+mv internal/cli/cli.go "/tmp/$NAME-$VERSION-main.go"
 
-cp main.go /tmp/nlp-main.go
-mv cmd/cli/main.go "/tmp/$NAME-$VERSION-main.go"
+go build -o chess cmd/cli/main.go
 
-go build -o chess cmd/cli/main.tmp.go
-
-rm cmd/cli/main.tmp.go
-mv "/tmp/$NAME-$VERSION-main.go" cmd/cli/main.go
+rm internal/cli/cli.tmp.go
+mv "/tmp/$NAME-$VERSION-main.go" internal/cli/cli.go

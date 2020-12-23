ba85ebd8016dcff6f9b465ee90f9b5fe2e5e19ea
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-12-23 01:21:54 +0100

    Added square values for knights

--- internal/engine/scoring.go
+++ internal/engine/scoring.go
@@ -39,7 +39,29 @@ var blackPawnValues = squareValues{
 	chess.A1: 0.0, chess.B1: 0.0, chess.C1: 0.0, chess.D1: 0.0, chess.E1: 0.0, chess.F1: 0.0, chess.G1: 0.0, chess.H1: 0.0,
 }
 
+var whiteKnightValues = squareValues{
+	chess.A8: -5.0, chess.B8: -4.0, chess.C8: -3.0, chess.D8: -3.0, chess.E8: -3.0, chess.F8: -3.0, chess.G8: -4.0, chess.H8: -5.0,
+	chess.A7: -4.0, chess.B7: -2.0, chess.C7: 0.0, chess.D7: 0.0, chess.E7: 0.0, chess.F7: 0.0, chess.G7: -2.0, chess.H7: -4.0,
+	chess.A6: -3.0, chess.B6: 0.0, chess.C6: 1.0, chess.D6: 1.5, chess.E6: 1.5, chess.F6: 1.0, chess.G6: 0.0, chess.H6: -3.0,
+	chess.A5: -3.0, chess.B5: 0.5, chess.C5: 1.5, chess.D5: 2.0, chess.E5: 2.0, chess.F5: 1.5, chess.G5: 0.5, chess.H5: -3.0,
+	chess.A4: -3.0, chess.B4: 0.0, chess.C4: 1.5, chess.D4: 2.0, chess.E4: 2.0, chess.F4: 1.0, chess.G4: 0.0, chess.H4: -3.0,
+	chess.A3: -3.0, chess.B3: 0.5, chess.C3: 1.0, chess.D3: 1.5, chess.E3: 1.5, chess.F3: 1.0, chess.G3: 0.5, chess.H3: -3.0,
+	chess.A2: -4.0, chess.B2: -2.0, chess.C2: 0.0, chess.D2: 0.5, chess.E2: 0.5, chess.F2: 0.0, chess.G2: -2.0, chess.H2: -4.0,
+	chess.A1: -5.0, chess.B1: -4.0, chess.C1: -3.0, chess.D1: -3.0, chess.E1: -3.0, chess.F1: -3.0, chess.G1: -4.0, chess.H1: -5.0,
+}
+
 var blackKnightValues = squareValues{
+	chess.A8: -5.0, chess.B8: -4.0, chess.C8: -3.0, chess.D8: -3.0, chess.E8: -3.0, chess.F8: -3.0, chess.G8: -4.0, chess.H8: -5.0,
+	chess.A7: -4.0, chess.B7: -2.0, chess.C7: 0.0, chess.D7: 0.5, chess.E7: 0.5, chess.F7: 0.0, chess.G7: -2.0, chess.H7: -4.0,
+	chess.A6: -3.0, chess.B6: 0.5, chess.C6: 1.0, chess.D6: 1.5, chess.E6: 1.5, chess.F6: 1.0, chess.G6: 0.5, chess.H6: -3.0,
+	chess.A5: -3.0, chess.B5: 0.0, chess.C5: 1.5, chess.D5: 2.0, chess.E5: 2.0, chess.F5: 1.0, chess.G5: 0.0, chess.H5: -3.0,
+	chess.A4: -3.0, chess.B4: 0.5, chess.C4: 1.5, chess.D4: 2.0, chess.E4: 2.0, chess.F4: 1.5, chess.G4: 0.5, chess.H4: -3.0,
+	chess.A3: -3.0, chess.B3: 0.0, chess.C3: 1.0, chess.D3: 1.5, chess.E3: 1.5, chess.F3: 1.0, chess.G3: 0.0, chess.H3: -3.0,
+	chess.A2: -4.0, chess.B2: -2.0, chess.C2: 0.0, chess.D2: 0.0, chess.E2: 0.0, chess.F2: 0.0, chess.G2: -2.0, chess.H2: -4.0,
+	chess.A1: -5.0, chess.B1: -4.0, chess.C1: -3.0, chess.D1: -3.0, chess.E1: -3.0, chess.F1: -3.0, chess.G1: -4.0, chess.H1: -5.0,
+}
+
+var whiteBishopValues = squareValues{
 	chess.A8: 0.0, chess.B8: 0.0, chess.C8: 0.0, chess.D8: 0.0, chess.E8: 0.0, chess.F8: 0.0, chess.G8: 0.0, chess.H8: 0.0,
 	chess.A7: 0.0, chess.B7: 0.0, chess.C7: 0.0, chess.D7: 0.0, chess.E7: 0.0, chess.F7: 0.0, chess.G7: 0.0, chess.H7: 0.0,
 	chess.A6: 0.0, chess.B6: 0.0, chess.C6: 0.0, chess.D6: 0.0, chess.E6: 0.0, chess.F6: 0.0, chess.G6: 0.0, chess.H6: 0.0,
@@ -50,7 +72,7 @@ var blackKnightValues = squareValues{
 	chess.A1: 0.0, chess.B1: 0.0, chess.C1: 0.0, chess.D1: 0.0, chess.E1: 0.0, chess.F1: 0.0, chess.G1: 0.0, chess.H1: 0.0,
 }
 
-var whiteKnightValues = squareValues{
+var blackBishopValues = squareValues{
 	chess.A8: 0.0, chess.B8: 0.0, chess.C8: 0.0, chess.D8: 0.0, chess.E8: 0.0, chess.F8: 0.0, chess.G8: 0.0, chess.H8: 0.0,
 	chess.A7: 0.0, chess.B7: 0.0, chess.C7: 0.0, chess.D7: 0.0, chess.E7: 0.0, chess.F7: 0.0, chess.G7: 0.0, chess.H7: 0.0,
 	chess.A6: 0.0, chess.B6: 0.0, chess.C6: 0.0, chess.D6: 0.0, chess.E6: 0.0, chess.F6: 0.0, chess.G6: 0.0, chess.H6: 0.0,
@@ -96,6 +118,8 @@ func scorePiece(piece chess.Piece, square chess.Square) float32 {
 	switch t {
 	case chess.Pawn:
 		value = calculatePawnValue(square, color)
+	case chess.Knight:
+		value = calculateKnightValue(square, color)
 	default:
 		value = calculateDefaultValue(t)
 	}
@@ -112,6 +136,18 @@ func calculatePawnValue(square chess.Square, color chess.Color) float32 {
 	}
 
 	squareValue, _ := whitePawnValues[square]
+	return pieceValue + squareValue
+}
+
+func calculateKnightValue(square chess.Square, color chess.Color) float32 {
+	pieceValue, _ := pieceValues[chess.Knight]
+
+	if color == chess.Black {
+		squareValue, _ := blackKnightValues[square]
+		return pieceValue + squareValue
+	}
+
+	squareValue, _ := whiteKnightValues[square]
 	return pieceValue + squareValue
 }
 
